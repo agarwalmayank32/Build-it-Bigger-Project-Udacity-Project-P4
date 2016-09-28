ea07c698859595caca01bfd184cdd4b3be2961da
mayank <agarwal.mayank32@gmail.com>
2016-09-28 17:52:49 +0530

    Initial Commit

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndPointAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndPointAsyncTaskTest.java
@@ -14,7 +14,7 @@ public class EndPointAsyncTaskTest {
     public void testDoInBackground() throws Exception {
         com.udacity.gradle.builditbigger.MainActivityFragment fragment = new com.udacity.gradle.builditbigger.MainActivityFragment();
         fragment.testFlag = true;
-        new EndpointAsyncTask().execute(fragment);
+        new EndpointAsyncTask().execute(fragment).get();
         Thread.sleep(5000);
         assertTrue("Error: Fetched Joke = " + fragment.loadedJoke, fragment.loadedJoke != null);
     }
